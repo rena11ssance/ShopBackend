d6ca4e24ac5a7e2ee48fd935db1db0d99eb909a7
she <lxst@list.ru>
2025-11-26 08:02:48 +0300

    added swagger

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapGet("/hello", () => "hello store api!");
